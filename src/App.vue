d14d5aa62baa209d823c44ca1b2adf05593c40b8
yasuaki640 <42242014+yasuaki640@users.noreply.github.com>
2021-05-29 22:37:53 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <!-- <BuefyForm /> -->
+    <RefactoredForm />
+  </div>
+</template>
+
+<script>
+import Form from "./components/Form";
+import RefactoredForm from "./components/RefactoredForm";
+
+export default {
+  name: "App",
+  components: {
+    BuefyForm: Form,
+    RefactoredForm
+  }
+};
+</script>
